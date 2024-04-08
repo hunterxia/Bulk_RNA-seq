1665bc492d9a7a1afa57700477fb884fd0c7e52a
shunranli <lshunran@gmail.com>
2024-04-08 00:07:35 -0500

    add modularization

---
+++ R/clustering.R
@@ -0,0 +1,16 @@
+clusteringTabServer <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    data <- reactive(mtcars[[input$var]])
+    output$hist <- renderPlot({
+      hist(data(), breaks = input$bins, main = input$var)
+    }, res = 96)
+  })
+}
+
+clusteringTabUI <- function(id) {
+  fluidPage(
+    selectInput(NS(id, "var"), "Variable", choices = names(mtcars)),
+    numericInput(NS(id, "bins"), "bins", value = 10, min = 1),
+    plotOutput(NS(id, "hist"))
+  )
+}
